21c0579cfbba4a9d4e16e7e4bd6827762245bb26
Sergi Casquero <111904117+Sergi-CM@users.noreply.github.com>
2023-11-28 21:37:07 +0100

    Fix cors access issue

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import partsRouter from "./routers/partsRouter/partsRouter.js";
+import cors from "cors";
 
 export const app = express();
 app.disable("x-powered-by");
 
+const options: cors.CorsOptions = {
+  origin: "http://localhost:5174",
+};
+
+app.use(cors(options));
 app.use(express.json());
 
 app.use("/parts", partsRouter);
