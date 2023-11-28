11a72c7c8ba162d1a4d81c10cec9e128427250ea
Sergi Casquero <111904117+Sergi-CM@users.noreply.github.com>
2023-11-28 17:54:02 +0100

    Add connection to database and console feedback

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+import connectDatabase from "./database/connectDatabase.js";
 import startServer from "./server/startServer.js";
 
 const port = process.env.PORT ?? 4000;
+const mongoDbUrl =
+  "mongodb+srv://Sergi:nmJKpBU0z9qPMoQ3@cluster0.bivdnqz.mongodb.net/?retryWrites=true&w=majority";
 
 try {
+  await connectDatabase(mongoDbUrl);
+  console.log("Connected to database");
+
   await startServer(4000);
   console.log(`Server listening on port ${port}`);
 } catch (error) {
